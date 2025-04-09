f6206065186352ca978ee8bb3432a01709590aad
erick mendez <eivan.mrmirez@protonmail.com>
2025-04-09 22:16:59 +0000

    test

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -5,7 +5,6 @@
  * @tree: A pointer to the root node of the tree to traverse.
  * @func: A pointer to a function to call for each node.
  */
-
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree && func)
